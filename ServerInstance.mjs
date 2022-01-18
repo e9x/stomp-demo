ecc2e346434249a0b360ba79176446d622726ee8
waterswat <waterswat@outlook.com>
2022-01-18 18:42:53 -0500

    add pm2 script, change loglevel if debugging

--- ServerInstance.mjs
+++ ServerInstance.mjs
@@ -1,11 +1,12 @@
 import { Server as TOMPServer, XORCodec, LOG_TRACE, LOG_WARN } from '../toomanyproxies/Server/index.mjs';
+import { Development } from '../toomanyproxies/WebpackUtil.mjs';
 import { Server as HTTPServer } from 'http';
 
 export const tompserver = new TOMPServer({
 	codec: XORCodec,
 	noscript: true,
 	prefix: '/tomp/',
-	loglevel: LOG_TRACE, // in prod, use LOG_WARN,
+	loglevel: Development ? LOG_TRACE : LOG_WARN,
 });
 
 export const httpserver = new HTTPServer();
---
+++ pm2.js
@@ -0,0 +1,4 @@
+// Run TOMP frontend under PM2
+'use strict';
+
+import('./index.mjs');
