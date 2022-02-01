c088e61aaeaaff99fa99d5cbff9196555d665301
waterswat <waterswat@outlook.com>
2022-01-31 19:02:17 -0500

    location test

--- run_tests.mjs
+++ run_tests.mjs
@@ -15,7 +15,7 @@ const tomp = new TOMP({
 
 const entries_dir = join(__dirname, 'public', 'Tests');
 
-const pick = 'Anchor';
+const pick = 'Location';
 const base = new URL('https://www.sys32.dev/');
 
 const fg_red = `\x1b[31m`;
